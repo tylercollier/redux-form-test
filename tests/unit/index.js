c53d78c546d7f3dc4484f9b671667f8a666de808
Tyler Collier <git@tylercollier.com>
2016-07-08 20:47:06 -0700

    Show that the solution is to use mount instead of shallow

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,87 +1,28 @@
-import ContactFormComponent from '../../app/ContactFormComponent'
 import React from 'react'
+import onClickOutside from 'react-onclickoutside'
 
 // See README for discussion of chai, enzyme, and sinon
 import chai, { expect } from 'chai'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
 import sinon from 'sinon'
 
 chai.use(chaiEnzyme())
 
-// In this file we're doing unit testing of our component, which means it
-// really has nothing to do with Redux-Form at this point. We can pass in our
-// own props (e.g. `submitting`) and make sure our form renders as we expect.
+
+
+
+// Note: This file was originally meant to show off unit testing the npm package named redux-form.
+// However, I've found it a convenient project to publicly show simple tests.
+// I've removed what was originally tested in this file to work with the question posted here:
+// http://stackoverflow.com/questions/38106763/using-mocha-to-test-higher-order-components-in-react
+
+
+
 
 describe("ContactFormComponent", () => {
-	let subject = null
-	let submitting, touched, error, resetForm, onSave, onSaveResponse
-	beforeEach(() => {
-		submitting = false
-		touched = false
-		error = null
-		resetForm = sinon.spy()
-		onSaveResponse = Promise.resolve()
-		onSave = sinon.stub()
-		onSave.returns(onSaveResponse)
-	})
-	const buildSubject = () => {
-		const props = {
-			onSave,
-			submitting: submitting,
-			// The real redux form has many properties for each field,
-			// including onChange and onBlur handlers. We only need to provide
-			// the ones that will change the rendered output.
-			fields: {
-				firstName: {
-					value: '',
-					touched: touched,
-					error: error
-				}
-			},
-			handleSubmit: fn => fn,
-			resetForm
-		}
-		return shallow(<ContactFormComponent {...props}/>)
-	}
-
-	// Here we show we can test asychronous actions triggered by our form.
-	it("calls resetForm after onSave", (done) => {
-		subject = buildSubject()
-		subject.find('form').simulate('submit')
-		expect(onSave.callCount).to.equal(1)
-		onSaveResponse.then(() => {
-			expect(resetForm.callCount).to.equal(1)
-			done()
-		})
-	})
-
-	// This is a very simle test, making sure that if we pass in a certain
-	// prop value, our form renders appropriately.
-	context("when submitting", () => {
-		it("shows a spinner while submitting", () => {
-			submitting = true
-			subject = buildSubject()
-			const icon = subject.find('button[type="submit"]').find('i')
-			expect(icon).to.have.className('glyphicon-refresh glyphicon-spin')
-		})
-	})
-
-	// Again, we show that what we render is based on the props we send it. If
-	// we set the props to include an error state, our form should render that
-	// fact.
-	context("when in an error state", () => {
-		it("renders an error message for the input", () => {
-			touched = true
-			error = "Required"
-			subject = buildSubject()
-			const firstNameHelpBlock = subject.find('.help-block')
-			expect(firstNameHelpBlock).to.exist
-			expect(firstNameHelpBlock.text()).to.equal('Required')
-		})
-	})
-
 	class Component extends React.Component {
+		handleClickOutside() {}
 		render () {
 			return (
 				<div className='component' />
@@ -98,20 +39,22 @@ describe("ContactFormComponent", () => {
 		});
 	})
 
-	const HOC = comp => {
-		return class myHOC extends React.Component {
-			render() {
-				return (
-					<comp/>
-				)
-			}
-		}
-	}
-	const HocComponent = HOC(Component)
+	const HocComponent = onClickOutside(Component)
 
-	describe('Component', function () {
+
+	// THIS TEST INTENTIONALLY FAILS! It shows what the OP witnessed in the stackoverflow question.
+	describe('Component wrapped in HOC, using shallow', function () {
 		it('can be mounted with the required class', function () {
 			const component = shallow(
+				<HocComponent />
+			);
+			expect(component).to.have.className('component');
+		});
+	})
+
+	describe('Component wrapped in HOC, using mount', function () {
+		it('can be mounted with the required class', function () {
+			const component = mount(
 				<HocComponent />
 			);
 			expect(component).to.have.className('component');
